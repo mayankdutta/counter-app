7c8540c2a2c932b36c4a1f92dc25a99f21d91ff9
Mayank Sharma <info@Mayanks-MacBook-Air.local>
2021-04-22 20:12:01 +0530

    Update state but with constructor method

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,21 +3,41 @@ import React, {Component} from 'react';
 class Counter extends Component {
   state = {
     count: 0,
-    imageURL: "https://picsum.photos/200"
   };
 
-  render() {
-    return (
-      <React.Fragment>
-        <img src = {this.state.imageURL}/>
-        <span> {this.formatCount()} </span>
-        <button className = "btn btn-primary btn-lg">Click Me </button>
-      </React.Fragment>
-  );
+  constructor() {
+    super();
+    this.increaseValue = this.increaseValue.bind(this);
   }
 
+  increaseValue() {
+    this.setState({count: this.state.count + 1});
+  }
+
+  /*
+  increaseValue() {
+    this.setState({
+      count: this.state.count + 1});
+  }
+  */
+
+  stylingBadge() {
+    let classToAdd = "badge m-2 badge-";
+    // if (this.state.count === 0) classToAdd += "danger";
+    // else classToAdd += "primary";
+
+    return (this.state.count === 0 ? classToAdd + "danger" : classToAdd + "primary");
+  }
+  render() {
+    return (
+      <div>
+        <span className = {this.stylingBadge()}> {this.formatCount()} </span>
+        <button onClick={this.increaseValue} className = "btn btn-secondary btn-sm" >Increment</button>
+      </div>
+  ); }
+
   formatCount() {
-    return this.state.count === 0 ? <h1>Zero</h1> : this.state.count;
+    return this.state.count === 0 ? "Zero" : this.state.count;
   }
 }
 
